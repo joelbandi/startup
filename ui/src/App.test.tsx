e1ba115ac9b54184dec2a0d77e956166dad2d378
Joel Bandi <joelvivekbandi@gmail.com>
2022-07-17 18:34:10 -0700

    Add test and lint support to ui service

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
+import { render, screen } from '@testing-library/react'
+import { App } from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const elem = screen.getByText(/Backend/i);
-  expect(elem).toBeInTheDocument();
+  render(<App />)
+  const elem = screen.getByText(/Backend/i)
+  expect(elem).toBeInTheDocument()
 });
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,16 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export const App = () => {
   const [text, setText] = useState('loading...')
   useEffect(() => {
     setTimeout(() => axios.get('api/').then((response) => setText(response.data.text)), 2000)
   })
-
   return (
     <div className="App">
-      Text from backend: {text}
+      Text from backend:
+      {' '}
+      {text}
     </div>
   )
 }
